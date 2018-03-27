778d9539744939199da3dbb2d32a840c5e828015
Luke Chambers <lukenatchambers@gmail.com>
2018-03-27 16:05:03 -0600

    Mess some things up just to mess with Andy

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,7 +1,9 @@
-class Api::V1::Games::Ships < ApiController
+class Api::V1::Games::ShipsController < ApiController
 
   def create
-
+    binding.pry
+    parsed = JSON.parse(params[:ship], symbolize_names: true)
+    ship = Ship.new(parsed)
   end
 
 end
