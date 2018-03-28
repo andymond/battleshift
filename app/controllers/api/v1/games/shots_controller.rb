1d48a911d6b1f2e4708da12b10258aa2e89203d0
Andrew Dymond <andy.dymond@gmail.com>
2018-03-27 18:15:21 -0600

    Add api_key_validation to shots_controller

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -2,14 +2,26 @@ module Api
   module V1
     module Games
       class ShotsController < ApiController
+        before_action :api_key_validation
+
         def create
           game = Game.find(params[:game_id])
 
           turn_processor = TurnProcessor.new(game, params[:shot][:target])
 
           turn_processor.run!
+
           render json: game, message: turn_processor.message
         end
+
+        private
+
+          def api_key_validation
+            not_found = {message: "Unauthorized"}
+            unless User.find_by(id: request.headers["X-API-KEY"])
+              render json: not_found, status: 401
+            end
+          end
       end
     end
   end
