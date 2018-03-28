be97e567a29c2659b0dacfc13b1f78f6543c5b45
Andrew Dymond <andy.dymond@gmail.com>
2018-03-27 18:18:36 -0600

    move api_key_validation to api_controller

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     module Games
       class ShotsController < ApiController
-        before_action :api_key_validation
 
         def create
           game = Game.find(params[:game_id])
@@ -14,14 +13,6 @@ module Api
           render json: game, message: turn_processor.message
         end
 
-        private
-
-          def api_key_validation
-            not_found = {message: "Unauthorized"}
-            unless User.find_by(id: request.headers["X-API-KEY"])
-              render json: not_found, status: 401
-            end
-          end
       end
     end
   end
