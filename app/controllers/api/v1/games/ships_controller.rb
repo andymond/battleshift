8b7410df77305570a89fb51762955a5296af042a
Andrew Dymond <andy.dymond@gmail.com>
2018-04-01 13:42:49 -0600

    call current_user, current_board & current_game in ships_controller to refactor

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -1,29 +1,27 @@
 class Api::V1::Games::ShipsController < ApiController
 
   def create
-    user = User.find(request.headers["HTTP_X_API_KEY"])
-    game = user.games.find(params[:game_id])
-    if game.player_1 == user.id
-      ship = Ship.new(params[:ship][:ship_size])
-      ship.place(params[:ship][:start_space], params[:ship][:end_space])
-      board = game.player_1_board
-      start_space = params[:ship][:start_space]
-      end_space = params[:ship][:end_space]
-      ShipPlacer.new(board: board, ship: ship, start_space: start_space, end_space: end_space).run
-      game.save!
-      message = Printer.new(game, 1).placed
-      render json: game, message: message
+    ship = Ship.new(params[:ship][:ship_size])
+    ship.place(params[:ship][:start_space], params[:ship][:end_space])
+    ShipPlacer.new(placement_params).run
+    current_game.save!
+    if current_game.player_1 == current_user.id
+      message = Printer.new(current_game, 1).placed
     else
-      ship = Ship.new(params[:ship][:ship_size])
-      ship.place(params[:ship][:start_space], params[:ship][:end_space])
-      board = game.player_2_board
-      start_space = params[:ship][:start_space]
-      end_space = params[:ship][:end_space]
-      ShipPlacer.new(board: board, ship: ship, start_space: start_space, end_space: end_space).run
-      game.save!
-      message = Printer.new(game, 2).placed
-      render json: game, message: message
+      message = Printer.new(current_game, 2).placed
     end
+    render json: current_game, message: message
   end
+
+  private
+
+    def placement_params
+      {
+        board: current_board,
+        ship: Ship.new(params[:ship][:ship_size]),
+        start_space: params[:ship][:start_space],
+        end_space: params[:ship][:end_space]
+      }
+    end
 
 end
