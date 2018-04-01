b103717c812ac066401043d548bc0ef3d63e943f
Andrew Dymond <andy.dymond@gmail.com>
2018-04-01 13:42:12 -0600

    Add current_user, current_board & current_game to api_controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,23 @@
 class ApiController < ActionController::API
-  before_action :api_key_validation
+  before_action :validate_api_key
 
-  private
+    def current_user
+      User.find_by(id: request.headers["HTTP_X_API_KEY"])
+    end
 
-    def api_key_validation
+    def current_game
+      @game ||= current_user.games.find(params[:game_id])
+    end
+
+    def current_board
+      if current_game.player_1 == current_user.id
+        current_game.player_1_board
+      else
+        current_game.player_2_board
+      end
+    end
+
+    def validate_api_key
       not_found = {message: "Unauthorized"}
       unless User.find_by(id: request.headers["X-API-KEY"])
         render json: not_found, status: 401
