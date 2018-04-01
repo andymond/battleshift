aba0eba9d7e81db8e3e5660869c895104ee19841
Andrew Dymond <andy.dymond@gmail.com>
2018-04-01 16:35:46 -0600

    Add #change_turn to turn_processor

--- app/services/turn_processor.rb
+++ app/services/turn_processor.rb
@@ -6,14 +6,6 @@ class TurnProcessor
     @messages = []
   end
 
-  def being_attacked
-    game.current_turn == "player_1" ? player_2 : player_1
-  end
-
-  def change_turn
-    game.current_turn == "player_1" ? game.current_turn = 1 : game.current_turn = 0
-  end
-
   def run!
     begin
       attack_opponent(being_attacked)
@@ -32,44 +24,68 @@ class TurnProcessor
 
   private
 
-  attr_reader :game, :target
+    attr_reader :game, :target
 
-  def attack_opponent(player)
-    result = Shooter.fire!(board: player.board, target: target)
-    @messages << "Your shot resulted in a #{result}."
-    @messages << "Battleship sunk." if sunk?(result, player.board)
-    @messages << "Game over." if sunk_count(player.board) == 5
-    game.player_1_board == player.board ? game.player_1_turns += 1 : game.player_2_turns += 1
-  end
+    def attack_opponent(player)
+      result = Shooter.fire!(board: player.board, target: target)
+      count_turn(player)
+      @messages << "Your shot resulted in a #{result}."
+      @messages << "Battleship sunk." if sunk?(result, player.board)
+      @messages << "Game over." if sunk_count(player.board) == 5
+    end
 
-  def ai_attack_back
-    result = AiSpaceSelector.new(player_1.board).fire!
-    @messages << "The computer's shot resulted in a #{result}."
-    game.player_2_turns += 1
-  end
-
-  def player_1
-    Player.new(game.player_1, game.player_1_board)
-  end
-
-  def player_2
-    Player.new(game.player_2, game.player_2_board)
-  end
-
-  def sunk?(result, board)
-    if result == "Hit"
-      board.board.flatten.any? do |space_hash|
-        space_hash.values[0].contents.is_sunk? if space_hash.keys == [target]
+    def being_attacked
+      if game.current_turn == "player_1"
+        player_2
+      else
+        player_1
       end
-    else
-      false
     end
-  end
 
-  def sunk_count(board)
-    board.board.flatten.count do |space_hash|
-      space_hash.values[0].contents.is_sunk? if space_hash.values[0].occupied?
+    def change_turn
+      if game.current_turn == "player_1"
+        game.current_turn = "player_2"
+      else
+        game.current_turn = "player_1"
+      end
     end
-  end
+
+    def count_turn(player)
+      if game.player_1_board == player.board
+        game.player_1_turns += 1
+      else
+        game.player_2_turns += 1
+      end
+    end
+
+    def ai_attack_back
+      result = AiSpaceSelector.new(player_1.board).fire!
+      @messages << "The computer's shot resulted in a #{result}."
+      game.player_2_turns += 1
+    end
+
+    def player_1
+      Player.new(game.player_1, game.player_1_board)
+    end
+
+    def player_2
+      Player.new(game.player_2, game.player_2_board)
+    end
+
+    def sunk?(result, board)
+      if result == "Hit"
+        board.board.flatten.any? do |space_hash|
+          space_hash.values[0].contents.is_sunk? if space_hash.keys == [target]
+        end
+      else
+        false
+      end
+    end
+
+    def sunk_count(board)
+      board.board.flatten.count do |space_hash|
+        space_hash.values[0].contents.is_sunk? if space_hash.values[0].occupied?
+      end
+    end
 
 end
