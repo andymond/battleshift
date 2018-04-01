63e098fc4f9bd9ea14f7282a52e1c6e207bb61d7
Andrew Dymond <andy.dymond@gmail.com>
2018-04-01 14:13:50 -0600

    Start refactoring shots_controller

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -2,34 +2,41 @@ module Api
   module V1
     module Games
       class ShotsController < ApiController
+        before_action :game_over?
+        before_action :current_turn
 
         def create
-          user_id = request.headers["HTTP_X_API_KEY"]
-          user = User.find(user_id)
-          game = user.games.find(params[:game_id])
-          current_player = user.player_number(game.id)
-          turn_processor = TurnProcessor.new(game, params[:shot][:target])
-          if game.winner
-            render json: game, status: 400, message: Printer.new(game, user).game_finished
+          turn_processor = TurnProcessor.new(current_game, params[:shot][:target])
+          turn_processor.run!
+          if turn_processor.message.include?("Game over")
+            current_game.update_attributes(winner: user.email)
+            render json: current_game, message: turn_processor.message
           else
-            if current_player == game.current_turn
-              turn_processor.run!
-              if turn_processor.message.include?("Game over")
-                game.update_attributes(winner: user.email)
-                render json: game, message: turn_processor.message
-              else
-                if turn_processor.message.include?("Invalid")
-                  render json: game , status: 400, message: turn_processor.message
-                else
-                  render json: game, message: turn_processor.message
-                end
-              end
+            if turn_processor.message.include?("Invalid")
+              render json: current_game , status: 400, message: turn_processor.message
             else
-              render json: game , status: 400, message: "Invalid move. It's your opponent's turn"
+              render json: current_game, message: turn_processor.message
             end
           end
         end
 
+        private
+
+          def game_over?
+            if current_game.winner
+              render json: current_game, status: 400, message: Printer.new(current_game, user).game_finished
+            end
+          end
+
+          def current_player
+            @current_player ||= current_user.player_number(current_game.id)
+          end
+
+          def current_turn
+            unless current_player == current_game.current_turn
+            render json: current_game , status: 400, message: "Invalid move. It's your opponent's turn"
+            end
+          end
       end
     end
   end
