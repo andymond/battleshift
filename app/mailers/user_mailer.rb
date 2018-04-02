823f2b45405a43e7a8c188ea1b536604312fbd88
Andrew Dymond <andy.dymond@gmail.com>
2018-04-01 21:09:51 -0600

    Modify user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,19 +3,19 @@ class UserMailer < ApplicationMailer
 
   def activation_invite(user)
     @user = user
-    @url = "#{ENV['root']}/activation/#{@user.id}"
+    @url = "#{ENV['ROOT']}/activation/#{@user.id}"
     mail(to: @user.email, subject: 'Activate your BattleShift account')
   end
 
   def game_invite(email, user)
     @user = user
-    @url = "#{ENV['root']}/games/#{user.games.last.id}"
+    @url = "#{ENV['ROOT']}/games/#{user.games.last.id}"
     mail(to: email, subject: "You have been invited to a game of BattleShift!")
   end
 
   def register_invite(email, user)
     @user = user
-    @url = "#{ENV['root']}/register"
+    @url = "#{ENV['ROOT']}/register"
     mail(to: email, subject: "You have been invited to a game of BattleShift!")
   end
 
