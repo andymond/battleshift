331a317074d83c9a6aefbc4da3822f7e46899bdf
Andrew Dymond <andy.dymond@gmail.com>
2018-04-01 19:07:27 -0600

    Fix spacing

--- spec/requests/api/v1/games_api_spec.rb
+++ spec/requests/api/v1/games_api_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "games api" do
-  let(:user) { create(:user) }
+  let(:user)         { create(:user) }
   let(:invited_user) { create(:user) }
   it "shows games index" do
     create_list(:game, 3)
