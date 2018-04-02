2446d94fd5ae0fe50dbc2871ed6b024f646956d5
Andrew Dymond <andy.dymond@gmail.com>
2018-04-01 20:22:05 -0600

    move action mailer config settings to environments/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 '587',
+    domain:               'example.com',
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { :host => "https://banana-tart-60248.herokuapp.com" }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
